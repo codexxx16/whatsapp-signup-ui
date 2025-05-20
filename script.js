a57d14be89650fd133a421571dfbe0a05a3201b2
codexxx16 <codexxxnull@gmail.com>
2025-05-20 12:55:09 +0200

    Update and rename script.js to verify.html

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("signupForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const phone = document.getElementById("phone").value.trim();
-  if (phone.length < 6) {
-    alert("Please enter a valid phone number.");
-    return;
-  }
-  window.location.href = "verify.html";
-});
